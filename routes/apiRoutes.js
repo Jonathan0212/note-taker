10828cfc6f2a18ef43f60b1c5d02ea862db76dc4
Your Name <Vaaax@Jon-PC>
2022-02-04 22:37:35 -0600

    added route folder and finalized routing code

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const store = require ('../db/store');
+
+router.get('/notes', (req,res) => {
+    store
+      .getNotes()
+      .then(notes => res.json(notes))
+      .catch(err => res.status(500).json(err))
+});
+
+router.post('/notes', (req,res) => {
+    store
+      .addNotes()
+      .then(notes => res.json(notes))
+      .catch(err => res.status(500).json(err))
+});
+
+router.delete('/notes', (req,res) => {
+  store
+  .removeNotes(req.params.id)
+  .then(() => res.json({ ok: true }))
+  .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+// USE apiRoutes
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT: ${PORT}`)
+})
